aa9ed623dfda7d63c59fb3199dc071affd0125b5
Louis <cheruiyotlouis@gmail.com>
2025-09-17 17:08:26 +0300

    Defines extensions initialization,CORS inside app.py

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
+from core.config import Config
+from core.extensions import db, bcrypt, jwt, migrate, api
+from apis.routes import register_routes
+from apis.stripe import init_stripe
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Init extensions
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+    api.init_app(app)
+
+    # CORS
+    CORS(app, resources={r"/*": {"origins": app.config["FRONTEND_URL"]}})
+
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
