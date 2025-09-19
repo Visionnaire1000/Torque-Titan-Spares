3fe5d58c386a742c8806ead070dc24716d6233e5
Louis <cheruiyotlouis@gmail.com>
2025-09-19 18:45:35 +0300

    Modifies api endpoints to match changes made to Resources

--- backend/apis/routes.py
+++ backend/apis/routes.py
@@ -1,19 +1,18 @@
 from apis.resources import (
     Register, Login, CreateAdmin,
-    SparePartsList, SpareParts,
-    Reviews, ReviewReactions,
-    Checkout, Orders, AdminOrders
+    SparePartsList,
+    ReviewsResource, ReviewReactionsResource,
+    Checkout, OrdersResource, AdminOrders
 )
 
 def register_routes(api):
     api.add_resource(Register, '/register')
     api.add_resource(Login, '/login')
     api.add_resource(CreateAdmin, '/admin/create')
-    api.add_resource(SparePartsList, '/spareparts')
-    api.add_resource(SpareParts, '/spareparts/<string:part_id>')
-    api.add_resource(Reviews, '/reviews/<string:part_id>', '/reviews/edit/<string:review_id>')
-    api.add_resource(ReviewReactions, '/reviews/<string:review_id>/react')
+    api.add_resource(SparePartsList, "/spareparts", "/spareparts/<string:part_id>")
+    api.add_resource(ReviewsResource, '/reviews/<string:part_id>', '/reviews/edit/<string:review_id>')
+    api.add_resource(ReviewReactionsResource, '/reviews/<string:review_id>/react')
     api.add_resource(Checkout, '/checkout')
-    api.add_resource(Orders, '/orders', '/orders/<string:order_id>')
+    api.add_resource(OrdersResource, '/orders', '/orders/<string:order_id>')
     api.add_resource(AdminOrders, '/admin/orders', '/admin/orders/<string:order_id>')
 
