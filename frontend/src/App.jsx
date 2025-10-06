00b2af05a1acf94bd53fba8ad02746c72ebd48d8
Louis <cheruiyotlouis@gmail.com>
2025-10-07 01:26:08 +0300

    Imports Login component into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { CartProvider } from "./contexts/CartContext";
 
 //pages
 import Register from "./components/Register";
+import Login from "./components/Login";
 import Homepage from "./components/Homepage";
 
 //style
@@ -24,6 +25,7 @@ function App() {
           <main className="flex-1">
             <Routes>
               <Route path="/register" element={<Register />} /> 
+              <Route path="/login" element={<Login />} /> 
               <Route path="/" element={<Homepage />} />
             </Routes>
           </main>
