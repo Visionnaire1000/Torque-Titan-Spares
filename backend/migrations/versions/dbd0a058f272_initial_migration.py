4d821f18844e912a6972860c9f1b261df12c79d3
Louis <cheruiyotlouis@gmail.com>
2025-10-08 12:10:22 +0300

    Modifies DB to include Reviews Table

--- backend/migrations/versions/dbd0a058f272_initial_migration.py
+++ backend/migrations/versions/dbd0a058f272_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: 0eb77054974a
+Revision ID: dbd0a058f272
 Revises: 
-Create Date: 2025-09-19 16:01:16.983908
+Create Date: 2025-10-08 12:06:26.457734
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0eb77054974a'
+revision = 'dbd0a058f272'
 down_revision = None
 branch_labels = None
 depends_on = None
